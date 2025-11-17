826accfbd7d5a51ad6321c884ea0d8e9d154dbc1
lowercasebtw <ignzoudy@gmail.com>
2025-11-17 01:25:47 -0500

    Add docs folder

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
 	title: "Animatium",
 	description: "Documentation on Animatium",
+	outDir: "./docs",
 	themeConfig: {
 		// https://vitepress.dev/reference/default-theme-config
 		nav: [{ text: "Home", link: "/" }],
